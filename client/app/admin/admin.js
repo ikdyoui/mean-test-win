bb9300a68978e96fc46e6255a32a63a0fba3a764
Yuichi Ikeda <ikeday@kita-osaka.co.jp>
2015-03-20 12:17:25 +0900

    first commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('meanTestWinApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
